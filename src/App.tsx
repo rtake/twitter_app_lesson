6bc60b1c54a88f63ef61cf06c757a12fc36a237f
rtake <rtake1110@gmail.com>
2023-08-02 03:29:54 +0900

    firebaseをインポート

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import styles from "./App.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function App() {
   return <div className="App"></div>;
