93fbc93875055505ae55a4f4d6b7254bd180c0a2
rtake <rtake1110@gmail.com>
2023-08-02 03:30:55 +0900

    AppをFunctional Componentに変更

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectUser, login, logout } from "./features/userSlice";
 import { auth } from "./firebase";
 
-function App() {
+const App: React.FC = () => {
   return <div className="App"></div>;
-}
+};
 
 export default App;
