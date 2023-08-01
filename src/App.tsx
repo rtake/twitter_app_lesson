d7308c3962be018fde78216dc1b460c4991368f1
rtake <rtake1110@gmail.com>
2023-08-02 04:12:09 +0900

    useEffect内での認証情報変更の監視を追加

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,27 @@ const App: React.FC = () => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    // 認証情報変更時に実行するコールバック関数を登録
+    // unSubにはこの関数をアンマウントするための関数が入る
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            diaplayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+
+    // クリーンアップ時にこの関数を実行
+    return unSub();
+  }, [dispatch]);
+
   return <div className="App"></div>;
 };
 
