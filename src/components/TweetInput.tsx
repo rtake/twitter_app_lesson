9b0d7bc2fc62020e61845d4fe71d6c09fc9313ef
rtake <rtake1110@gmail.com>
2023-08-02 20:20:50 +0900

    TweetInputにRedux Storeから取得したユーザーデータを表示する

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -3,9 +3,21 @@ import styles from "./TweetInput.module.css";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
 import { auth } from "../firebase";
+import { Avatar } from "@material-ui/core";
 
 const TweetInput: React.FC = () => {
-  return <div></div>;
+  const user = useSelector(selectUser);
+  return (
+    <div>
+      <Avatar
+        className={styles.tweet_avatar}
+        src={user.photoUrl}
+        onClick={async () => {
+          await auth.signOut();
+        }}
+      />
+    </div>
+  );
 };
 
 export default TweetInput;
