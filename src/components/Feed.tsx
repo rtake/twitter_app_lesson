f39e19c2710421c9e1709062a5dbcf703f0aea81
rtake <rtake1110@gmail.com>
2023-08-02 20:09:29 +0900

    FeedにTweetInputを埋め込み

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { auth } from "../firebase";
+import TweetInput from "./TweetInput";
 
 const Feed: React.FC = () => {
   return (
     <div>
       Feed
+      <TweetInput />
       <button
         onClick={() => {
           auth.signOut();
