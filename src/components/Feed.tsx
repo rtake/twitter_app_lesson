7d4f3c809e6049cfe52f4e5f4db28f4dd9719e1e
rtake <rtake1110@gmail.com>
2023-08-02 16:31:39 +0900

    Logoutボタンを実装

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { auth } from "../firebase";
 
 const Feed: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      Feed
+      <button
+        onClick={() => {
+          auth.signOut();
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Feed;
