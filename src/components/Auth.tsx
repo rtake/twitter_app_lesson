405825340ee3d6d410b392bb0e0a5128837815af
rtake <rtake1110@gmail.com>
2023-08-02 14:29:24 +0900

    Material UIのimportを修正

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -2,19 +2,26 @@ import React, { useState } from "react";
 import styles from "./Auth.module.css";
 import { useDispatch } from "react-redux";
 import { auth, storage, provider } from "../firebase";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Paper,
+  Grid,
+  Typography,
+  makeStyles,
+  Modal,
+  IconButton,
+  Box,
+} from "@material-ui/core";
+
+import SendIcon from "@material-ui/icons";
+import CameraIcon from "@material-ui/icons";
+import EmailIcon from "@material-ui/icons";
+import LockOutlinedIcon from "@material-ui/icons";
+import AccountCircleIcon from "@material-ui/icons";
 
 function Copyright() {
   return (
