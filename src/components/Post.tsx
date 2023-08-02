d89ab754abb0d56de7033a3d111866ea498f2d2e
rtake <rtake1110@gmail.com>
2023-08-03 00:16:42 +0900

    投稿を表示するPostコンポーネントを新規作成

---
+++ src/components/Post.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import styles from "./Post.module.css";
+import { db } from "../firebase";
+import firebase from "firebase/app";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { Avatar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import MssageIcon from "@material-ui/icons/Message";
+import SendIcon from "@material-ui/icons/Send";
+
+const Post: React.FC = () => {
+  return <div></div>;
+};
+
+export default Post;
