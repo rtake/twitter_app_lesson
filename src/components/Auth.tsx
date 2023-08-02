1da10e65e3db0c5e461b1f11b23503ccf0588b1e
rtake <rtake1110@gmail.com>
2023-08-02 14:37:21 +0900

    トップページの画像を変更

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -28,7 +28,8 @@ const useStyles = makeStyles((theme) => ({
     height: "100vh",
   },
   image: {
-    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundImage:
+      "url(https://images.unsplash.com/photo-1618826411640-d6df44dd3f7a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80)",
     backgroundRepeat: "no-repeat",
     backgroundColor:
       theme.palette.type === "light"
